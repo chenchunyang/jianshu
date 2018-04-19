34dab3dcc0e9b4eed930b28247e713d1fb1f0fd2
lizhiyuan <148334733@qq.com>
2018-04-19 10:18:32 +0800

    登录页面已经全部做完了

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -47,17 +47,17 @@
                 <ul>
                     <li>
                         <nuxt-link to="/">
-                            <i class="fa fa-weixin"></i>
+                            <i class="weixin fa fa-weixin"></i>
                         </nuxt-link>
                     </li>
                     <li>
                         <nuxt-link to="/">
-                            <i class="fa fa-weibo"></i>
+                            <i class="weibo fa fa-weibo"></i>
                         </nuxt-link>
                     </li>
                     <li>
                         <nuxt-link to="/">
-                            <i class="fa fa-qq"></i>
+                            <i class="qq fa fa-qq"></i>
                         </nuxt-link>
                     </li>
                 </ul>
